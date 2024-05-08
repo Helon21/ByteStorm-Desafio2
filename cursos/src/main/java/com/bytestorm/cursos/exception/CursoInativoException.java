bf51bd50e0d82a9477d446d7215f359ee1502680
Helon21 <helonbentes51@hotmail.com>
2024-05-08 14:47:30 -0300

    fix: consertando repositório

---
+++ cursos/src/main/java/com/bytestorm/cursos/exception/CursoInativoException.java
@@ -0,0 +1,8 @@
+package com.bytestorm.cursos.exception;
+
+public class CursoInativoException extends RuntimeException {
+
+    public CursoInativoException(String msg) {
+        super(msg);
+    }
+}
